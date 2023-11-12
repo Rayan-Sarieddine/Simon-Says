6ff0b630f7ce9400c56b73120c2fc89b041593b0
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-11-12 11:48:08 +0200

    added user functionality

--- game.js
+++ game.js
@@ -1,17 +1,24 @@
 "use strict";
+//buttons
 const green = document.querySelector("#green");
 const red = document.querySelector("#red");
 const yellow = document.querySelector("#yellow");
 const blue = document.querySelector("#blue");
 const buttons = document.querySelectorAll(".btn");
+
+//sounds
 const greenSound = new Audio("sounds/green.mp3");
 const redSound = new Audio("sounds/red.mp3");
 const yellowSound = new Audio("sounds/yellow.mp3");
 const blueSound = new Audio("sounds/blue.mp3");
 const wrongSound = new Audio("sounds/wrong.mp3");
+
+//UI elements
 const message = document.querySelector("#level-title");
 const body = document.querySelector("body");
+const container = document.querySelector(".container");
 
+//messages
 const loseMessage = "Game Over, Press Any Key to Restart";
 const initialMessage = "Press Any Key to Start";
 const winMessage = "Congrats! You Won. Press Any Key to Restart";
@@ -63,3 +70,65 @@ function ComputerTurn(iter) {
 
   message.innerHTML = `Level ${level}`;
 }
+
+//user part
+container.addEventListener("click", function (e) {
+  const choice = e.target.classList[1];
+
+  const index = userAnswer.push(choice) - 1;
+  document.querySelector(`#${choice}`).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(`#${choice}`).classList.remove("pressed");
+  }, 100);
+  const sound = document.querySelector(`#${choice}`);
+  if (sound.classList.contains("green")) {
+    greenSound.play();
+  } else if (sound.classList.contains("red")) {
+    redSound.play();
+  } else if (sound.classList.contains("yellow")) {
+    yellowSound.play();
+  } else {
+    blueSound.play();
+  }
+
+  if (userAnswer[index] !== answer[index]) {
+    message.innerHTML = loseMessage;
+    answer = [];
+    userAnswer = [];
+    level = 0;
+    iter = 1;
+    body.classList.add("red");
+    setTimeout(function () {
+      body.classList.remove("red");
+      body.addEventListener("click", first);
+    }, 200);
+    return;
+  }
+
+  if (userAnswer.length == answer.length) {
+    if (level == 10) {
+      message.innerHTML = winMessage;
+      answer = [];
+      userAnswer = [];
+      level = 0;
+      iter = 1;
+      setTimeout(function () {
+        body.addEventListener("click", first);
+      }, 1000);
+    } else {
+      userAnswer = [];
+      setTimeout(function () {
+        iter++;
+        ComputerTurn(iter);
+      }, 1000);
+      return;
+    }
+  }
+});
+
+function first() {
+  body.removeEventListener("click", first);
+  ComputerTurn(1);
+}
+
+body.addEventListener("click", first);
