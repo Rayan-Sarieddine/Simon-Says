4766358330c853388e11008406d3c3e8b3610c8e
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-11-12 08:46:04 +0200

    implemented needed variables

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 "use strict";
+const green = document.querySelector("#green");
+const red = document.querySelector("#red");
+const yellow = document.querySelector("#yellow");
+const blue = document.querySelector("#blue");
+const buttons = document.querySelectorAll(".btn");
+const greenSound = new Audio("sounds/green.mp3");
+const redSound = new Audio("sounds/red.mp3");
+const yellowSound = new Audio("sounds/yellow.mp3");
+const blueSound = new Audio("sounds/blue.mp3");
+const wrongSound = new Audio("sounds/wrong.mp3");
+const message = document.querySelector("#level-title");
+const body = document.querySelector("body");
+
+const loseMessage = "Game Over, Press Any Key to Restart";
+const initialMessage = "Press Any Key to Start";
+const winMessage = "Congrats! You Won. Press Any Key to Restart";
